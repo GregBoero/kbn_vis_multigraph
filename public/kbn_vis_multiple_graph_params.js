fca3eceaa4f03075e3478ee0443b36d72880ba86
gregory <gregory.boeroteyssier@mythalesgroup.com>
2018-02-14 14:39:29 +0100

    - end of the refactoring of chart option :   - restore the logic of the dynamic option

--- public/kbn_vis_multiple_graph_params.js
+++ public/kbn_vis_multiple_graph_params.js
@@ -1,5 +1,7 @@
 import {uiModules} from 'ui/modules';
 import tableVisParamsTemplate from 'plugins/kbn_vis_multiple_graph/kbn_vis_multiple_graph_params.html';
+import _ from 'lodash';
+
 
 uiModules.get('kibana/kbn_vis_multiple_graph')
   .directive('kbnVisMultipleGraphParams', () => {
@@ -8,7 +10,7 @@ uiModules.get('kibana/kbn_vis_multiple_graph')
       template: tableVisParamsTemplate,
       link: ($scope) => {
 
-        if(!$scope.$root.charts_option || ($scope.$root.charts_option && $scope.$root.charts_option.length < 1)){
+        if (!$scope.$root.charts_option || ($scope.$root.charts_option && $scope.$root.charts_option.length < 1)) {
           $scope.$root.charts_option = $scope.vis.params.charts_option;
         }
 
@@ -17,31 +19,27 @@ uiModules.get('kibana/kbn_vis_multiple_graph')
         };
 
         $scope.shouldShowHidePoint = () => {
-          return false;
-          //   if (!$rootScope.label_keys) {
-          //       return false;
-          //   }
-          // const lineChartLabel = 'line';
-          // const splineChartLabel = 'spline';
-          // //TODO don't right all the chart type iterate over them
-          // return ($rootScope.label_keys.length > 0 && ($scope.vis.params.type1.includes(lineChartLabel) || $scope.vis.params.type1.includes(splineChartLabel))) ||
-          //   ($rootScope.label_keys.length > 1 && ($scope.vis.params.type2.includes(lineChartLabel) || $scope.vis.params.type2.includes(splineChartLabel))) ||
-          //   ($rootScope.label_keys.length > 2 && ($scope.vis.params.type3.includes(lineChartLabel) || $scope.vis.params.type3.includes(splineChartLabel))) ||
-          //   ($rootScope.label_keys.length > 3 && ($scope.vis.params.type4.includes(lineChartLabel) || $scope.vis.params.type4.includes(splineChartLabel))) ||
-          //   ($rootScope.label_keys.length > 4 && ($scope.vis.params.type5.includes(lineChartLabel) || $scope.vis.params.type5.includes(splineChartLabel)));
+          const lineChartLabel = 'line';
+          const splineChartLabel = 'spline';
+
+          let result = false;
+          _.each($scope.$root.charts_option, (chartOpt) => {
+            result |= chartOpt.type.includes(lineChartLabel) || chartOpt.type.includes(splineChartLabel);
+          });
+
+          return result;
+
         };
 
         $scope.shouldShowGrouped = () => {
-          return false
-          // //TODO don't right all the chart type iterate over them
-          // const chartType = [$scope.vis.params.type1, $scope.vis.params.type2, $scope.vis.params.type3, $scope.vis.params.type4, $scope.vis.params.type5];
-          // let numberOfBarChart = 0 ;
-          // chartType.forEach((type) =>{
-          //   if(type === 'bar'){
-          //     numberOfBarChart++;
-          //   }
-          // });
-          // return numberOfBarChart >= 2;
+          let result = 0;
+          _.each($scope.$root.charts_option, (option) => {
+
+            if (option.type === 'bar') {
+              result++;
+            }
+          });
+          return (result >= 2);
         }
       }
     };
